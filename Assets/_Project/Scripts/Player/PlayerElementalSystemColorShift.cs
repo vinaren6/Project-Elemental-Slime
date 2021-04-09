51dc3c96ae193e5cc93c5d21bcc8d49b56caf4f7
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-09 16:27:01 +0200

    Finished player color shift

--- Assets/_Project/Scripts/Player/PlayerElementalSystemColorShift.cs
+++ Assets/_Project/Scripts/Player/PlayerElementalSystemColorShift.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using _Project.Scripts.ElementalSystem;
 using UnityEngine;
 
 namespace _Project.Scripts.Player
 {
 	public class PlayerElementalSystemColorShift : MonoBehaviour
 	{
-		public void OnElementalTypeChange()
+		[SerializeField] private PlayerElementalSystemTypeCurrent type;
+
+		private Renderer _renderer;
+		private void     Start() => _renderer = gameObject.GetComponent<Renderer>();
+
+		public void StartColorShift() =>
+			StartCoroutine(UpdateColor(ElementalSystemColors.GetColorFromElementalSystemTypes(type.Type)));
+
+		private IEnumerator UpdateColor(Color targetColor)
 		{
-			throw new NotImplementedException();
-		}
-		/*
-		IEnumerator UpdateColor(Color targetColor)
-		{
-			while (expression)
-			{
+			while (_renderer.material.color != targetColor) {
+				_renderer.material.color = Color.Lerp(
+					_renderer.material.color, targetColor, 0.01f + Time.deltaTime);
 				yield return null;
 			}
-		}*/
+		}
 	}
 }
