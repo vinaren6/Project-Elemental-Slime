bda5bba1be74b8224caf17fb290b684c48b367e8
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-04-13 10:54:04 +0200

    Added SOAudioEvent scripts

---
+++ Assets/_Project/Scripts/Audio/AudioEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu]
+public class AudioEvent : ScriptableObject 
+{
+	public AudioClip[] clips;
+
+	public RangedFloat volume;
+
+	[MinMaxRange(0, 2)]
+	public RangedFloat pitch;
+
+	public void Play(AudioSource audioSource)
+	{
+		if (audioSource.isPlaying) return;
+		if (clips.Length == 0) return;
+
+		audioSource.clip = clips[Random.Range(0, clips.Length)];
+		audioSource.volume = Random.Range(volume.minValue, volume.maxValue);
+		audioSource.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+		audioSource.Play();
+	}
+}
