6abedf9e0657b81d5888a281884b1013d6a38b77
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-13 09:19:50 +0200

    Added Trigger (script and prefab)

---
+++ Assets/_Project/Scripts/Trigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Project.Scripts
+{
+	public class Trigger : MonoBehaviour
+	{
+		public  UnityEvent onTriggerEnter;
+		private void       OnTriggerEnter(Collider other) { onTriggerEnter.Invoke(); }
+	}
+}
