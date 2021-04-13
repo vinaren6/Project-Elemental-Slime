1e326638ddd708fb0d95f555ae8a2e692e7c0b9f
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-04-13 10:54:27 +0200

    Code CleanUp

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -2,53 +2,52 @@ using UnityEngine;
 
 namespace _Project.Scripts.Player
 {
-    public class PlayerMovement : MonoBehaviour
-    {
-        [Header("Movement Configuration")] [SerializeField]
-        private float movementSpeed = 0.25f;
+	public class PlayerMovement : MonoBehaviour
+	{
+		[Header("Movement Configuration")]
+		[SerializeField] private float movementSpeed = 0.25f;
+		
+		private Rigidbody _rb;
+		private float     _verticalInput;
+		private float     _horizontalInput;
+		private Camera    _mainCamera;
+		private Vector3   _forward, _right;
 
-        private Rigidbody rb;
-        private float verticalInput;
-        private float horizontalInput;
+		private void Start()
+		{
+			_rb         = GetComponent<Rigidbody>();
+			_mainCamera = Camera.main;
+			SetIsometricReferences();
+		}
 
-        private Camera mainCamera;
-        private Vector3 forward, right;
-        
-        private void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-            mainCamera = Camera.main;
-            SetIsometricReferences();
-        }
+		private void FixedUpdate()
+		{
+			GetInput();
+			MovePlayer();
+		}
 
-        private void FixedUpdate()
-        {
-            GetInput();
-            MovePlayer();
-        }
+		private void SetIsometricReferences()
+		{
+			_forward   = _mainCamera.transform.forward;
+			_forward.y = 0;
+			_forward   = Vector3.Normalize(_forward);
+			_right     = Quaternion.Euler(new Vector3(0, 90, 0)) * _forward;
+		}
 
-        private void SetIsometricReferences()
-        {
-            forward = mainCamera.transform.forward;
-            forward.y = 0;
-            forward = Vector3.Normalize(forward);
-            right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
-        }
+		private void GetInput()
+		{
+			_horizontalInput = Input.GetAxisRaw("Horizontal");
+			_verticalInput   = Input.GetAxisRaw("Vertical");
+		}
 
-        private void GetInput()
-        {
-            horizontalInput = Input.GetAxisRaw("Horizontal");
-            verticalInput = Input.GetAxisRaw("Vertical");
-        }
-
-        private void MovePlayer()
-        {
-            if (horizontalInput == 0 && verticalInput == 0)
-                return;
-            Vector3 rightMovement = right * horizontalInput;
-            Vector3 upMovement = forward * verticalInput;
-            Vector3 movementDirection = Vector3.Normalize(rightMovement + upMovement);
-            rb.MovePosition(rb.position + movementDirection * movementSpeed);
-        }
-    }
+		private void MovePlayer()
+		{
+			if (_horizontalInput == 0 && _verticalInput == 0)
+				return;
+			Vector3 rightMovement     = _right   * _horizontalInput;
+			Vector3 upMovement        = _forward * _verticalInput;
+			Vector3 movementDirection = Vector3.Normalize(rightMovement + upMovement);
+			_rb.MovePosition(_rb.position + movementDirection * movementSpeed);
+		}
+	}
 }
