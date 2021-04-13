c28234ba5fc983b5348076d96d47c6fdfbb151c5
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-13 11:03:50 +0200

    Updated Triggers

--- Assets/_Project/Scripts/Trigger.cs
+++ Assets/_Project/Scripts/Trigger.cs
@@ -6,6 +6,8 @@ namespace _Project.Scripts
 	public class Trigger : MonoBehaviour
 	{
 		public  UnityEvent onTriggerEnter;
+		public  UnityEvent onTriggerExit;
 		private void       OnTriggerEnter(Collider other) { onTriggerEnter.Invoke(); }
+		private void       OnTriggerExit(Collider  other) { onTriggerEnter.Invoke(); }
 	}
 }
