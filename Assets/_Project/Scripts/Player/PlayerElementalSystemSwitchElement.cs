0a3fead7d7c51d4a2715c988e7a6b0b4ef0b1410
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-14 10:17:32 +0200

    added PlayerElementalSystemSwitchElement

---
+++ Assets/_Project/Scripts/Player/PlayerElementalSystemSwitchElement.cs
@@ -0,0 +1,29 @@
+﻿using _Project.Scripts.ElementalSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project.Scripts.Player
+{
+	public class PlayerElementalSystemSwitchElement : MonoBehaviour
+	{
+		[SerializeField] private InputActionAsset           controls;
+		[SerializeField] private ElementalSystemTypeCurrent elementType;
+
+		private InputActionMap _inputActionMap;
+		private void Awake()
+		{
+			_inputActionMap = controls.FindActionMap("Element Controls");
+			_inputActionMap.FindAction("Earth").performed += _ => Switch(0);
+			_inputActionMap.FindAction("Wind").performed  += _ => Switch(1);
+			_inputActionMap.FindAction("Water").performed += _ => Switch(2);
+			_inputActionMap.FindAction("Fire").performed  += _ => Switch(3);
+			_inputActionMap.FindAction("Base").performed  += _ => Switch(4);
+		}
+
+		private void OnEnable() => _inputActionMap.Enable();
+
+		private void OnDisable() => _inputActionMap.Disable();
+
+		private void Switch(int type) => elementType.Type = (ElementalSystemTypes) type;
+	}
+}
