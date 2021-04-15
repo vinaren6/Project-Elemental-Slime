9f3c29b4287d0462e0dd121f8bef4bf20f3f3226
Emilie <emivog0223@skola.goteborg.se>
2021-04-15 15:35:41 +0200

    Update ProjectileController.cs

--- Assets/_Project/Scripts/Player/ProjectileController.cs
+++ Assets/_Project/Scripts/Player/ProjectileController.cs
@@ -7,6 +7,10 @@ namespace _Project.Scripts.Player
 {
     public class ProjectileController : MonoBehaviour
     {
-
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.collider.CompareTag("Player")) { return; }
+            gameObject.SetActive(false);
+        }
     }
 }
