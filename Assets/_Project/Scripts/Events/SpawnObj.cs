9120057d1d32db471cef0b6cf391475684cb6695
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-15 11:43:31 +0200

    added a Instantiate class

---
+++ Assets/_Project/Scripts/Events/SpawnObj.cs
@@ -0,0 +1,19 @@
+﻿using _Project.Scripts.ElementalSystem;
+using UnityEngine;
+
+namespace _Project.Scripts.Events
+{
+	public class SpawnObj : MonoBehaviour
+	{
+
+		[SerializeField] private GameObject                 prefab;
+		[SerializeField] private ElementalSystemTypeCurrent type;
+		
+		public void Spawn()
+		{
+			GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity);
+			if (obj.TryGetComponent(out ElementalSystemTypeCurrent prefabType))
+				prefabType.Type = type.Type;
+		}
+	}
+}
