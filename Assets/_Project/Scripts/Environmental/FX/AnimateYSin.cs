9d590a2d703329e59b532b868df55d963baaf521
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-15 10:56:44 +0200

    Added AnimeYSin.cs

---
+++ Assets/_Project/Scripts/Environmental/FX/AnimateYSin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Environmental.FX
+{
+	public class AnimateYSin : MonoBehaviour
+	{
+		[SerializeField] private float strenght = 1;
+		[Range(0, 10f)]
+		[SerializeField] private float speed = 1;
+
+		private void FixedUpdate()
+		{
+			transform.localPosition = new Vector3(
+				transform.localPosition.x, 
+				Mathf.Sin(Time.fixedTime * speed) * strenght, 
+				transform.localPosition.z);
+		}
+	}
+}
