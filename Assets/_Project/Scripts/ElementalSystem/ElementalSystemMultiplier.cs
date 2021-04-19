58d023d93e6bfbad0f0ab2a5692bacf521c5ea28
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-19 11:41:25 +0200

    Update ElementalSystemMultiplier.cs

--- Assets/_Project/Scripts/ElementalSystem/ElementalSystemMultiplier.cs
+++ Assets/_Project/Scripts/ElementalSystem/ElementalSystemMultiplier.cs
@@ -2,10 +2,27 @@
 {
 	public static class ElementalSystemMultiplier
 	{
+		/*
 		public static float GetMultiplier(ElementalSystemTypes baseType, ElementalSystemTypes targetType) =>
 			baseType == ElementalSystemTypes.Base || targetType == ElementalSystemTypes.Base
 				? 1
 				: 1 + ((int) baseType - (int) targetType) % 2;
+				*/
+		public static float GetMultiplier(ElementalSystemTypes baseType, ElementalSystemTypes targetType)
+		{
+			switch (baseType) {
+				case ElementalSystemTypes.Earth when targetType == ElementalSystemTypes.Fire:
+					return 2;
+				case ElementalSystemTypes.Fire when targetType == ElementalSystemTypes.Earth:
+					return .5f;
+				default: {
+					float mult = baseType == ElementalSystemTypes.Base || targetType == ElementalSystemTypes.Base
+						? 1
+						: 1 + ((int) baseType - (int) targetType) % 2;
+					return mult == 0 ? .5f : mult;
+				}
+			}
+		}
 	}
 }
 
