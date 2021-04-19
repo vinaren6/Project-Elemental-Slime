bce231e9de2a8d349e73ca3277059d924f395e93
Viktor Rüdén <viktor.ruden2@gmail.com>
2021-04-19 15:06:35 +0200

    made a script so player can only go through object with right element

---
+++ Assets/_Project/Scripts/Environmental/ElementalTerrain.cs
@@ -0,0 +1,37 @@
+using _Project.Scripts.ElementalSystem;
+using _Project.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalTerrain : MonoBehaviour
+{
+        [SerializeField] private bool equal = true;
+        [SerializeField] private ElementalSystemTypes type;
+        [SerializeField] private GameObject target;
+        [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerElementalSystemSwitchElement.StaticPlayerElementType.onElementTypeChange.AddListener(Change);
+        Change();
+    }
+
+    // Update is called once per frame
+    public void Change()
+    {
+       
+
+        for (int i = 0; i < System.Enum.GetValues(typeof(ElementalSystemTypes)).Length; i++)
+        {
+            if (type == PlayerElementalSystemSwitchElement.StaticPlayerElementType.Type)
+            {
+                Physics.IgnoreCollision(player.GetComponent<BoxCollider>(), target.GetComponent<BoxCollider>(), true);
+            }
+            else
+            {
+                Physics.IgnoreCollision(player.GetComponent<BoxCollider>(), target.GetComponent<BoxCollider>(), false);
+            }
+        }
+    }
+}
