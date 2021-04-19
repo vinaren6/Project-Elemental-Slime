47588d77895ca2f6f42e7fc08645241fd92db28e
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-19 10:26:43 +0200

    Added Restart.cs

---
+++ Assets/_Project/Scripts/Events/Restart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Events
+{
+	public class Restart : MonoBehaviour
+	{
+		public void RestartNow() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
