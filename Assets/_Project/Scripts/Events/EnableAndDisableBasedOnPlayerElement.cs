21adc1bdf4ea471496e20b7386d11ae0b05aadbd
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-19 11:26:19 +0200

    added EnableAndDisableBasedOnPlayerElement.cs

---
+++ Assets/_Project/Scripts/Events/EnableAndDisableBasedOnPlayerElement.cs
@@ -0,0 +1,24 @@
+﻿using _Project.Scripts.ElementalSystem;
+using _Project.Scripts.Player;
+using UnityEngine;
+
+namespace _Project.Scripts.Events
+{
+	public class EnableAndDisableBasedOnPlayerElement : MonoBehaviour
+	{
+		[SerializeField] private bool                 equal = true;
+		[SerializeField] private ElementalSystemTypes type;
+		[SerializeField] private GameObject           target;
+
+		private void Start()
+		{
+			PlayerElementalSystemSwitchElement.StaticPlayerElementType.onElementTypeChange.AddListener(Change);
+			Change();
+		}
+
+		private void Change() => target.SetActive(
+			equal
+				? type == PlayerElementalSystemSwitchElement.StaticPlayerElementType.Type
+				: type != PlayerElementalSystemSwitchElement.StaticPlayerElementType.Type);
+	}
+}
