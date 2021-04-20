ba4a14c97e4ec4e50b6a22f17649e26ebd1f031d
Emilie <emivog0223@skola.goteborg.se>
2021-04-20 10:41:32 +0200

    Elements bar modify

--- Assets/_Project/Scripts/UI/ElementBarUI.cs
+++ Assets/_Project/Scripts/UI/ElementBarUI.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using _Project.Scripts.ElementalSystem;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +6,17 @@ namespace _Project.Scripts.UI
 {
     public class ElementBarUI : MonoBehaviour
     {
+        
+        [SerializeField] private Image backgroundImage;
         [SerializeField] private Image fillImage;
         [SerializeField] private Image highlightImage;
-
+        [SerializeField] private ElementalSystemTypes type;
+        [SerializeField] private Color[] elementColors;
+        
         private void Awake()
         {
             highlightImage.enabled = false;
+            SetupColors();
         }
 
         public void UpdateElementPercent(float percent)
@@ -22,6 +25,11 @@ namespace _Project.Scripts.UI
 
             highlightImage.enabled = percent >= 1f;
         }
-    }
 
+        private void SetupColors()
+        {
+            backgroundImage.color = elementColors[(int) type];
+            // highlightImage.color = elementColors[(int) type * 2 + 1];
+        }
+    }
 }
