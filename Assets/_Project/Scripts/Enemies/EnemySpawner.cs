08ba38188c1fec95abc899f4644b27b5e1ca2f90
BakaRecca <eddie.norberg@gmail.com>
2021-04-22 12:50:55 +0200

    EnemySpawner by Type (Instantiate)

---
+++ Assets/_Project/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using _Project.Scripts.ElementalSystem;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts.Enemies
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private ElementalSystemTypes type;
+        [SerializeField][Range(1f, 5f)] private float minSpawnDelay;
+        [SerializeField][Range(5f, 10f)] private float maxSpawnDelay;
+        [SerializeField] private Color[] colors;
+        
+        private Transform _transform;
+
+        private void Awake()
+        {
+            name = $"EnemySpawner [{type}]";
+            _transform = transform;
+
+            int index = 0;
+            
+            MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer meshRenderer in meshRenderers)
+            {
+                Color color = colors[(int) type];
+
+                if (index == 0)
+                    color.a = 0.5f;
+                
+                meshRenderer.material.color = color;
+                index++;
+            }
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SpawnRoutine());
+        }
+
+        private IEnumerator SpawnRoutine()
+        {
+            yield return new WaitForSeconds(GetSpawnDelay());
+
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            GameObject enemy = Instantiate(enemyPrefab, _transform.position, Quaternion.identity, _transform);
+            enemy.GetComponent<ElementalSystemTypeCurrent>().Type = type;
+            enemy.name = $"Enemy[{type}]";
+
+            StartCoroutine(SpawnRoutine());
+        }
+
+        private float GetSpawnDelay()
+        {
+            return Random.Range(minSpawnDelay, maxSpawnDelay);
+        }
+    }
+}
