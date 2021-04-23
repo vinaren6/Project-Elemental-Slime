12abe9f1a85b0139f1ad8b03ef1ab2fd678eea6e
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-04-23 10:14:40 +0200

    Update EnemySpawner.cs

--- Assets/_Project/Scripts/Enemies/EnemySpawner.cs
+++ Assets/_Project/Scripts/Enemies/EnemySpawner.cs
@@ -51,6 +51,8 @@ namespace _Project.Scripts.Enemies
             if (enemiesInScene < maxEnemiesInScene) {
                 enemiesInScene++;
                 Spawn();
+            } else {
+                StartCoroutine(SpawnRoutine());
             }
         }
 
