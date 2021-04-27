b33c25733a529d522b656a3a594f995063012624
Eddie Norberg <eddie.norberg@gmail.com>
2021-04-27 15:58:03 +0200

    Water Ability Laser basic functionality.

---
+++ Assets/_Project/Scripts/Abilities/IAbility.cs
@@ -0,0 +1,7 @@
+namespace _Project.Scripts.Abilities
+{
+    public interface IAbility
+    {
+        void Execute();
+    }
+}
---
+++ Assets/_Project/Scripts/Abilities/WaterAbility.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project.Scripts.Abilities
+{
+    public class WaterAbility : MonoBehaviour, IAbility
+    {
+        [SerializeField] private LayerMask collisionMask;
+        private LineRenderer _laser;
+        private ParticleSystem _splashEffect;
+        private Transform _transform;
+        private Transform _splashEffectTransform;
+        private Transform _splashEffectParent;
+        private float _maxDistance;
+        private float _radius;
+        private bool _splashHasStopped;
+
+        private void Awake()
+        {
+            GetAllComponents();
+            Initialize();
+        }
+
+        private void Update()
+        {
+            if (Mouse.current.leftButton.isPressed)
+                Execute();
+            else
+                _laser.SetPosition(1, _laser.GetPosition(0));
+            
+            if (!Mouse.current.leftButton.isPressed && _splashEffect.isPlaying && !_splashHasStopped)
+            {
+                // Debug.Log($"Splash Has Stopped! Soon....");
+                
+                Invoke(nameof(ResetSplashEffect), _splashEffect.main.duration);
+                _splashHasStopped = true;
+            }
+        }
+
+        private void GetAllComponents()
+        {
+            _laser = GetComponentInChildren<LineRenderer>();
+            _splashEffect = GetComponentInChildren<ParticleSystem>();
+            _transform = transform;
+            _splashEffectTransform = _splashEffect.transform;
+            _splashEffectParent = _splashEffectTransform.parent;
+        }
+
+        private void Initialize()
+        {
+            _maxDistance = 25f;
+            _radius = _laser.startWidth * 0.5f;
+            _splashEffect.Stop();
+            _laser.SetPosition(1, _laser.GetPosition(0));
+        }
+
+        public void Execute()
+        {
+            Vector3 origin = _transform.position;
+            Vector3 direction = _transform.forward;
+
+            RaycastHit hit;
+            
+            Vector3 hitPosition = _laser.GetPosition(0);
+            
+            if (Physics.CapsuleCast(origin, origin, _radius, direction, out hit, _maxDistance, collisionMask))
+            {
+                Transform hitTransform = hit.collider.transform;
+                
+                hitPosition.z = hit.distance;
+
+                _splashEffectTransform.SetParent(hitTransform);
+                _splashEffectTransform.localPosition = Vector3.zero;
+                float rotY = _transform.eulerAngles.y - 180f;
+                _splashEffectTransform.localRotation = Quaternion.Euler(0f, rotY, 0f);
+
+                _splashHasStopped = false;
+
+                PlayEffect();
+            }
+            else
+            {
+                hitPosition.z = _maxDistance;
+                StopEffect();
+            }
+
+            _laser.SetPosition(1, hitPosition);
+        }
+
+        private void ResetSplashEffect()
+        {
+            _splashEffect.Stop();
+            _splashEffectTransform.SetParent(_splashEffectParent);
+        }
+
+        private void PlayEffect()
+        {
+            if (_splashEffect.isPlaying)
+                return;
+            
+            _splashEffect.Play();
+            Invoke(nameof(ResetSplashEffect), _splashEffect.main.duration);
+        }
+
+        private void StopEffect()
+        {
+            if (!_splashEffect.isPlaying)
+                return;
+            
+            Invoke(nameof(ResetSplashEffect), _splashEffect.main.duration);
+        }
+    }
+}
