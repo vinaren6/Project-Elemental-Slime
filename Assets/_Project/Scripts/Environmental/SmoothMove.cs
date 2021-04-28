e9b3736505bcdd4f1a7c75bda8f01f2294cc0b76
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-04-28 16:58:18 +0200

    finished Level001

--- Assets/_Project/Scripts/Environmental/SmoothMove.cs
+++ Assets/_Project/Scripts/Environmental/SmoothMove.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace _Project.Scripts.Environmental
 {
 	public class SmoothMove : MonoBehaviour
 	{
-		private                  Vector3 _targetPos = new Vector3();
-		private                  Vector3 _t         = new Vector3();
 		[SerializeField] private float   smoothTime = 1f;
+		private                  Vector3 _t;
+		private                  Vector3 _targetPos;
 
 		private void Update()
 		{
@@ -15,7 +14,8 @@ namespace _Project.Scripts.Environmental
 				enabled = false;
 				return;
 			}
-			transform.position = Vector3.SmoothDamp(transform.position, _targetPos, ref _t,smoothTime);
+
+			transform.position = Vector3.SmoothDamp(transform.position, _targetPos, ref _t, smoothTime);
 		}
 
 		public void GoToo(Vector3 target)
@@ -23,6 +23,7 @@ namespace _Project.Scripts.Environmental
 			_targetPos = target;
 			enabled    = true;
 		}
+
 		public void GoTooX(float target) => GoToo(new Vector3(target, transform.position.y, transform.position.z));
 		public void GoTooY(float target) => GoToo(new Vector3(transform.position.x, target, transform.position.z));
 		public void GoTooZ(float target) => GoToo(new Vector3(transform.position.x, transform.position.y, target));
