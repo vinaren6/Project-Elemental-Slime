81f55b99708ad0afe4923c54880f4c6b798d4f6c
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-05-03 13:20:54 +0200

    Added a portal to level001

---
+++ Assets/_Project/Scripts/Events/LoadSceneEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Events
+{
+	public class LoadSceneEvent : MonoBehaviour
+	{
+		public void LoadScene(int         buildIndex) => SceneManager.LoadScene     (buildIndex);
+		public void LoadScene(string      sceneName ) => SceneManager.LoadScene     (sceneName );
+		public void LoadSceneAsync(int    buildIndex) => SceneManager.LoadSceneAsync(buildIndex);
+		public void LoadSceneAsync(string sceneName ) => SceneManager.LoadSceneAsync(sceneName );
+	}
+}
