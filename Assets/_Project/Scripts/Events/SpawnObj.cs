d9c2a7484b21df5e5f5bdddca548eb67e75c64da
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-05-17 23:30:04 +0200

    Added SelfDestruction to Drops

--- Assets/_Project/Scripts/Events/SpawnObj.cs
+++ Assets/_Project/Scripts/Events/SpawnObj.cs
@@ -9,11 +9,20 @@ namespace _Project.Scripts.Events
 		[SerializeField] private GameObject                 prefab;
 		[SerializeField] private ElementalSystemTypeCurrent type;
 		
+		public bool  isSelfDestructing;
+		public float selfDestructTimer;
+		
+		
 		public void Spawn()
 		{
 			GameObject obj = Instantiate(prefab, transform.position + Vector3.up * 0.5f, Quaternion.identity);
 			if (obj.TryGetComponent(out ElementalSystemTypeCurrent prefabType))
 				prefabType.Type = type.Type;
+			
+			if (isSelfDestructing) {
+				if (obj.TryGetComponent(out DestroyThisObj destroyThisObj))
+					destroyThisObj.DestroyMe(selfDestructTimer);
+			}
 		}
 	}
 }
