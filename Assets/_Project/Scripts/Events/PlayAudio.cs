454fae558d2e5b6459c1c7514cc1752cf3b57e74
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-05-17 15:20:51 +0200

    Update PlayAudio.cs

--- Assets/_Project/Scripts/Events/PlayAudio.cs
+++ Assets/_Project/Scripts/Events/PlayAudio.cs
@@ -1,35 +1,38 @@
 using System;
-using UnityEngine;
+using _Project.Scripts.Audio.ScriptableObjects;
 using _Project.Scripts.Managers;
+using UnityEngine;
 using AudioType = _Project.Scripts.Audio.AudioType;
 
 namespace _Project.Scripts.Events
 {
-    public class PlayAudio : MonoBehaviour
-    {
-        [SerializeField] private AudioClip audioClip;
-        [SerializeField] private AudioType audioType;
-        [SerializeField] private float     volume = 1;
-        [SerializeField] private bool      playOnStart;
+	public class PlayAudio : MonoBehaviour
+	{
+		[SerializeField] private AudioClip audioClip;
+		[SerializeField] private AudioType audioType;
 
-        private void Start()
-        {
-            if (playOnStart) Play();
-        }
+		[Space] [MinMaxRange(0.001f, 1f)] [SerializeField]
+		private float volume = 1;
 
-        public void Play()
-        {
-            switch (audioType)
-            {
-                case AudioType.SFX:
-                    ServiceLocator.Audio.PlaySFX(audioClip, volume);
-                    break;
-                case AudioType.BGM:
-                    ServiceLocator.Audio.PlayBGM(audioClip, volume);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-    }
+		[SerializeField] private bool playOnStart;
+
+		private void Start()
+		{
+			if (playOnStart) Play();
+		}
+
+		public void Play()
+		{
+			switch (audioType) {
+				case AudioType.SFX:
+					ServiceLocator.Audio.PlaySFX(audioClip, volume);
+					break;
+				case AudioType.BGM:
+					ServiceLocator.Audio.PlayBGM(audioClip, volume);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
 }
