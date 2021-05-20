c693c4bcb8b5bf4d9eae9657fc7b2a841b42a843
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-05-20 14:53:04 +0200

    cleenup

--- Assets/_Project/Scripts/UI/Menu/MainMenuPanelType.cs
+++ Assets/_Project/Scripts/UI/Menu/MainMenuPanelType.cs
@@ -1,10 +1,10 @@
 namespace _Project.Scripts.UI.Menu
 {
-    public enum MainMenuPanelType
-    {
-        TitleScreen,
-        Options,
-        Credits,
-        Stats
-    }
+	public enum MainMenuPanelType
+	{
+		TitleScreen,
+		Options,
+		Credits,
+		Stats
+	}
 }
--- Assets/_Project/Scripts/UI/PauseMenuPanelType.cs
+++
@@ -1,8 +0,0 @@
-namespace _Project.Scripts.UI
-{
-    public enum PauseMenuPanelType
-    {
-        PauseScreen,
-        Options
-    }
-}
