7f6d45a9aa41a28f93d4139f349aade4f0d8cb6b
Linus-Jonsson <70511818+Linus-Jonsson@users.noreply.github.com>
2021-05-21 10:11:54 +0200

    Remove SpecialAttack from IHUD & NamingFixes

--- Assets/_Project/Scripts/OLD/PlayerSpecialAttackOLD.cs
+++ Assets/_Project/Scripts/OLD/PlayerSpecialAttackOLD.cs
@@ -6,20 +6,20 @@ namespace _Project.Scripts.Player
 {
 	public class PlayerSpecialAttackOLD : MonoBehaviour
 	{
-		[SerializeField] private AudioClip specialSFX;
-		private float _nextSpecialAttack = 0;
-		
-		public void Activate(GameObject specialAttack, float projectileSpeed, float specialAttackCooldownTime, ElementalSystemTypeCurrent type)
-		{
-			if (Time.time < _nextSpecialAttack)
-				return;
-			
-			GameObject projectile = Instantiate(specialAttack, transform.position, Quaternion.identity);
-			projectile.GetComponent<ElementalSystemTypeCurrent>().Type = type.Type;
-			projectile.GetComponent<Rigidbody>().AddForce(transform.forward * (projectileSpeed * 1.2f));
-			_nextSpecialAttack = Time.time + specialAttackCooldownTime;
-			ServiceLocator.HUD.SpecialAttack?.StartCooldown(specialAttackCooldownTime);
-			ServiceLocator.Audio.PlaySFX(specialSFX);
-		}
+		// [SerializeField] private AudioClip specialSFX;
+		// private float _nextSpecialAttack = 0;
+		//
+		// public void Activate(GameObject specialAttack, float projectileSpeed, float specialAttackCooldownTime, ElementalSystemTypeCurrent type)
+		// {
+		// 	if (Time.time < _nextSpecialAttack)
+		// 		return;
+		// 	
+		// 	GameObject projectile = Instantiate(specialAttack, transform.position, Quaternion.identity);
+		// 	projectile.GetComponent<ElementalSystemTypeCurrent>().Type = type.Type;
+		// 	projectile.GetComponent<Rigidbody>().AddForce(transform.forward * (projectileSpeed * 1.2f));
+		// 	_nextSpecialAttack = Time.time + specialAttackCooldownTime;
+		// 	ServiceLocator.HUD.SpecialAttack?.StartCooldown(specialAttackCooldownTime);
+		// 	ServiceLocator.Audio.PlaySFX(specialSFX);
+		// }
 	}
 }
--- Assets/_Project/Scripts/UI/PlayerHUD.cs
+++ Assets/_Project/Scripts/UI/PlayerHUD.cs
@@ -6,30 +6,27 @@ namespace _Project.Scripts.UI
 {
     public class PlayerHUD : MonoBehaviour, IHUD
     {
-        [SerializeField] private HealthUI _healthbar;
-        [SerializeField] private SpecialAttackUIOLD _specialAttack;
-        [SerializeField] private ElementBarUI[] _elementBars;
+        [SerializeField] private HealthUI       healthBar;
+        [SerializeField] private ElementBarUI[] elementBars;
 
-        public HealthUI Healthbar => _healthbar;
+        public HealthUI HealthBar => healthBar;
+        
+        private void Start()
+        {
+            ServiceLocator.ProvideHUD(this);
+        }
         
         public void UpdateElementBar(ElementalSystemTypes type, float percent)
         {
             if (type == ElementalSystemTypes.Base)
                 return;
             
-            _elementBars[(int)type].UpdateUI(percent);
+            elementBars[(int)type].UpdateUI(percent);
         }
 
-        public SpecialAttackUIOLD SpecialAttack => _specialAttack;
-
-        private void Start()
-        {
-            ServiceLocator.ProvideHUD(this);
-        }
-        
         public ElementBarUI ElementBar(ElementalSystemTypes type)
         {
-            return _elementBars[(int) type];
+            return elementBars[(int) type];
         }
     }
 }
