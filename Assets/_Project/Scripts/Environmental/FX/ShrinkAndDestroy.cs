4563fcfef0da53e5cde93dbeb25a6474c002474d
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2021-05-21 12:35:12 +0200

    Added a shinking effect to drops

---
+++ Assets/_Project/Scripts/Environmental/FX/ShrinkAndDestroy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Environmental.FX
+{
+	public class ShrinkAndDestroy : MonoBehaviour
+	{
+		[SerializeField] private Transform target;
+		[SerializeField] private float     time = 5;
+		private                  float     _t;
+		private                  Vector3   _baseScale;
+
+		private void Start()
+		{
+			_baseScale        = target.localScale;
+			target.localScale = Vector3.zero;
+		}
+
+		private void FixedUpdate()
+		{
+			_t += Time.fixedDeltaTime;
+			if (_t >= time)
+				Destroy(gameObject);
+			else
+				target.localScale = _baseScale * (1 - _t / time);
+		}
+	}
+}
--- Assets/_Project/Scripts/Events/SpawnObj.cs
+++ Assets/_Project/Scripts/Events/SpawnObj.cs
@@ -5,24 +5,14 @@ namespace _Project.Scripts.Events
 {
 	public class SpawnObj : MonoBehaviour
 	{
-
 		[SerializeField] private GameObject                 prefab;
 		[SerializeField] private ElementalSystemTypeCurrent type;
-		
-		public bool  selfDestroy;
-		public float selfDestroyDelay;
-		
-		
+
 		public void Spawn()
 		{
 			GameObject obj = Instantiate(prefab, transform.position + Vector3.up * 0.5f, Quaternion.identity);
 			if (obj.TryGetComponent(out ElementalSystemTypeCurrent prefabType))
 				prefabType.Type = type.Type;
-			
-			if (selfDestroy) {
-				if (obj.TryGetComponent(out DestroyThisObj destroyThisObj))
-					destroyThisObj.DestroyMe(selfDestroyDelay);
-			}
 		}
 	}
 }
