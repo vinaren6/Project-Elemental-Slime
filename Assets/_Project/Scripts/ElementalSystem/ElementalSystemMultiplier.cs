08dd95e4d56258a20ae382df5ea2781f748c6320
Eddie Norberg <eddie.norberg@gmail.com>
2021-05-26 13:11:05 +0200

    DamageMultiplier works as intended again.

--- Assets/_Project/Scripts/ElementalSystem/ElementalSystemMultiplier.cs
+++ Assets/_Project/Scripts/ElementalSystem/ElementalSystemMultiplier.cs
@@ -1,4 +1,6 @@
-﻿namespace _Project.Scripts.ElementalSystem
+﻿using UnityEngine;
+
+namespace _Project.Scripts.ElementalSystem
 {
 	public static class ElementalSystemMultiplier
 	{
@@ -10,18 +12,35 @@
 				*/
 		public static float GetMultiplier(ElementalSystemTypes baseType, ElementalSystemTypes targetType)
 		{
-			switch (baseType) {
-				case ElementalSystemTypes.Fire when targetType == ElementalSystemTypes.Wind:
-					return 2;
-				case ElementalSystemTypes.Wind when targetType == ElementalSystemTypes.Fire:
-					return .5f;
-				default: {
-					float mult = baseType == ElementalSystemTypes.Base || targetType == ElementalSystemTypes.Base
-						? 1
-						: 1 + ((int) baseType - (int) targetType) % 2;
-					return mult == 0 ? .5f : mult;
-				}
-			}
+			return targetType switch
+			{
+				ElementalSystemTypes.Fire when baseType == ElementalSystemTypes.Earth => 2f,
+				ElementalSystemTypes.Fire when baseType == ElementalSystemTypes.Water => 0.5f,
+				ElementalSystemTypes.Fire => 1f,
+				ElementalSystemTypes.Water when baseType == ElementalSystemTypes.Fire => 2f,
+				ElementalSystemTypes.Water when baseType == ElementalSystemTypes.Wind => 0.5f,
+				ElementalSystemTypes.Water => 1f,
+				ElementalSystemTypes.Earth when baseType == ElementalSystemTypes.Wind => 2f,
+				ElementalSystemTypes.Earth when baseType == ElementalSystemTypes.Fire => 0.5f,
+				ElementalSystemTypes.Earth => 1f,
+				ElementalSystemTypes.Wind when baseType == ElementalSystemTypes.Water => 2f,
+				ElementalSystemTypes.Wind when baseType == ElementalSystemTypes.Earth => 0.5f,
+				ElementalSystemTypes.Wind => 1f,
+				_ => 1f
+			};
+
+			// switch (baseType) {
+			// 	case ElementalSystemTypes.Fire when targetType == ElementalSystemTypes.Wind:
+			// 		return 2;
+			// 	case ElementalSystemTypes.Wind when targetType == ElementalSystemTypes.Fire:
+			// 		return .5f;
+			// 	default: {
+			// 		float mult = baseType == ElementalSystemTypes.Base || targetType == ElementalSystemTypes.Base
+			// 			? 1
+			// 			: 1 + ((int) baseType - (int) targetType) % 2;
+			// 		return mult == 0 ? .5f : mult;
+			// 	}
+			// }
 		}
 	}
 }
@@ -31,3 +50,6 @@
 // 2 - Earth,
 // 3 - Wind,
 // 4 - Base
+
+// baseType = the characterType that gets hit
+// targetType = the attackType that deals damage
